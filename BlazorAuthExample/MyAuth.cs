051550be0afd1f24a442c03b1ffcbf11c746d7c3
Peter Morris <mrpmorris@gmail.com>
2024-10-12 21:22:51 +0100

    Fully working

--- BlazorAuthExample/MyAuth.cs
+++ BlazorAuthExample/MyAuth.cs
@@ -3,7 +3,9 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
+using System.Diagnostics.CodeAnalysis;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 
@@ -47,10 +49,14 @@ public class MyAuthHandler : RemoteAuthenticationHandler<MyAuthOptions>
 
     private string BuildChallengeUrl(AuthenticationProperties properties)
     {
+        GenerateCorrelationId(properties);
+
         var stateDictionary = new Dictionary<string, string?>();
 
         if (properties.Items.TryGetValue("XsrfId", out string? xsrfId))
             stateDictionary["XsrfId"] = xsrfId;
+
+        stateDictionary[".xsrf"] = properties.Items[".xsrf"];
 
         if (!properties.Items.TryGetValue(".redirect", out string? redirect))
             redirect = properties.RedirectUri;
@@ -58,9 +64,7 @@ public class MyAuthHandler : RemoteAuthenticationHandler<MyAuthOptions>
 
         stateDictionary["LoginProvider"] = "MyAuth";
 
-        string stateJson = JsonSerializer.Serialize(stateDictionary);
-        string stateJsonProtected = GetDataProtector().Protect(stateJson);
-
+        string stateJsonProtected = ProtectState(stateDictionary);
         string endpoint = "/my-sign-in-page";
         string result = QueryHelpers.AddQueryString(endpoint, "state", stateJsonProtected);
         return result;
@@ -74,17 +78,56 @@ public class MyAuthHandler : RemoteAuthenticationHandler<MyAuthOptions>
 
         var ticket = new AuthenticationTicket(principal, "myauth");
 
-        string stateJsonProtected = Context.Request.Form["state"]!;
-        string stateJson = GetDataProtector().Unprotect(stateJsonProtected);
-        var stateDictionary = JsonSerializer.Deserialize<Dictionary<string, string?>>(stateJson)!;
+        if (!Context.Request.Form.TryGetValue("State", out StringValues stateJsonProtected)
+            || !UnprotectState(stateJsonProtected, out Dictionary<string, string?> stateDictionary))
+        {
+            return Task.FromResult(HandleRequestResult.Fail("State was missing or invalid."));
+        }
 
         ticket.Properties.RedirectUri = stateDictionary["MyAuth_Redirect"];
-        foreach(var kvp in stateDictionary.Where(x => !x.Key.StartsWith("MyAuth_")))
+        foreach (var kvp in stateDictionary.Where(x => !x.Key.StartsWith("MyAuth_")))
             ticket.Properties.Items[kvp.Key] = kvp.Value;
+
+
+        if (!ValidateCorrelationId(ticket.Properties))
+            return Task.FromResult(HandleRequestResult.Fail("Correlation failed."));
 
         return Task.FromResult(HandleRequestResult.Success(ticket));
     }
 
+    private bool UnprotectState(
+        string? stateJsonProtected,
+        [NotNullWhen(true)]
+        out Dictionary<string, string?> state)
+    {
+        if (stateJsonProtected is null)
+        {
+            state = null;
+            return false;
+        }
+
+        try
+        {
+            string stateJson = GetDataProtector().Unprotect(stateJsonProtected);
+            state = JsonSerializer.Deserialize<Dictionary<string, string?>>(stateJson)!;
+            return state is not null;
+        }
+        catch (CryptographicException)
+        {
+            state = null;
+            return false;
+        }
+    }
+
     private IDataProtector GetDataProtector() =>
         DataProtectionProvider.CreateProtector("MyAuth");
+
+    private string ProtectState(Dictionary<string, string?> stateDictionary)
+    {
+        string stateJson = JsonSerializer.Serialize(stateDictionary);
+        string stateJsonProtected = GetDataProtector().Protect(stateJson);
+        return stateJsonProtected;
+    }
+
+
 }
